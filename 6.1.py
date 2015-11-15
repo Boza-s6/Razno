bbe32fcf6153c2476613bb9c1104b5c370f06426
Boza-s6 <nemanja.boki92@gmail.com>
2015-11-15 23:04:55 +0100

    Izbrisani python skripte. Vec se nalaze u pytrhon diru

--- 6.1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-
-with open("razno.txt", 'r') as fajl, open("indetifikatori.txt", "w") as izlaz:
-	#read file, make list of lines (without '\n'), join with space between, split by spaces to get words
-	reci = " ".join([x for x in fajl.read().splitlines()]).split()
-	iden = [x for x in reci if x[:1].isalpha() or x.startswith("_")]
-	izlaz.write(" ".join(iden))
